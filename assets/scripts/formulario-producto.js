4a642648520c5b836ddd9285e603a51bbe99bc14
SergioCE13 <sergioce.ipn@gmail.com>
2024-05-03 15:53:52 -0600

    Verificación de los archivos

--- assets/scripts/formulario-producto.js
+++ assets/scripts/formulario-producto.js
@@ -1,5 +1,4 @@
 /* Traemos los componentes que vamos a utilizar para realizar la validación de los campos */
-
 const inputNombre = document.getElementById('input-nombre-producto');
 const inputPrecio = document.getElementById('input-precio-producto');
 const inputDescripcion = document.getElementById('textArea-desc-producto');
@@ -7,6 +6,13 @@ const inputInfoAd = document.getElementById('textArea-info-adicional');
 const inputStock = document.getElementById('input-stock-producto');
 const buttonSubmit = document.getElementById("button-submit");
 
+// Deshabilitamos el botón del formularo ya que incialmente los campos están vacíos.
+buttonSubmit.classList.add('button-disabled');
+buttonSubmit.disabled = true;
+
+    // Ocultar ambas alertas al principio
+    document.getElementById("alerta-success").style.display = "none";
+    document.getElementById("alerta-danger").style.display = "none";
 
 // Obtenemos los valores que contienen los campos de entrada.
 const nombreProducto =  inputNombre.value;
@@ -15,19 +21,17 @@ const descripcionProducto = inputDescripcion.value;
 const infoAdicionalProducto = inputInfoAd.value;
 const stockProducto = inputStock.value;
 
-
-// Deshabilitamos el botón del formularo ya que incialmente los campos están vacíos.
-buttonSubmit.classList.add('button-disabled');
-buttonSubmit.disabled = true;
-
-
-function validarCampos(){   
+function validarCampos(){
+    const nombreProducto = inputNombre.value;
     if(nombreProducto !== ""){
         buttonSubmit.classList.remove('button-disabled');
         buttonSubmit.disabled = false;
+        document.getElementById("alerta-success").style.display = "block";
+        document.getElementById("info-nombre-producto").style.display = "none";
     } else {
         buttonSubmit.classList.add('button-disabled');
         buttonSubmit.disabled = true;
+        document.getElementById("alerta-danger").style.display = "block";
     }
 }
 
@@ -35,4 +39,29 @@ function validarCampos(){
     // Agregamos escucha de eventos a cada uno de los campos de entrada para llamar a la función validarCampos cuando se ingrese  o modifique texto:
     inputNombre.addEventListener('input', validarCampos);
 
+
     
+        const productoJSON = { 
+            "nombreProducto" : nombreProducto,
+            "validaPrecio" : validaPrecio,
+            "validaDescripcion" : validaDescripcion,
+            "validaInfoAd" : validaInfoAd,
+            "validaStock" : validaStock 
+        };
+    
+
+    /*
+        Función para obtener el valor seleccionado de los radio buttons
+        const radios = document.getElementsByName("inlineRadioOptions")
+    function obtenerCategoriaSeleccionada(radios) {
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                return radios[i].value;
+                //return 
+            }
+        }
+        return null; // Si ninguno está seleccionado
+    }
+
+    para llamar a la función ocuparemos un obtenerCategoriaSeleccionada(radios);
+*/
