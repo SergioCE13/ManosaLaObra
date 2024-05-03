0f76ce36a4c56eb042a8e699c55c0bd60e32f081
dsalvatori <davidgsalvatori@gmail.com>
2024-05-03 13:36:41 -0600

    Añadí vista previa de las fotos que suban los usuarios y añadi inputs para las dimensiones del producto.

--- assets/scripts/formulario-producto.js
+++ assets/scripts/formulario-producto.js
@@ -31,8 +31,41 @@ function validarCampos(){
     }
 }
 
+// Agregamos escucha de eventos a cada uno de los campos de entrada para llamar a la función validarCampos cuando se ingrese  o modifique texto:
+inputNombre.addEventListener('input', validarCampos);
 
-    // Agregamos escucha de eventos a cada uno de los campos de entrada para llamar a la función validarCampos cuando se ingrese  o modifique texto:
-    inputNombre.addEventListener('input', validarCampos);
-
+//Funcion que muestra vista previa de las fotos cargadas por el usuario
+   
+function previewFiles() {
+        const preview = document.querySelector("#preview");
+        const files = document.querySelector("input[type=file]").files;
+      
+        function readAndPreview(file) {
+          // Make sure `file.name` matches our extensions criteria
+          if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+            const reader = new FileReader();
+      
+            reader.addEventListener(
+              "load",
+              () => {
+                const image = new Image();
+                image.height = 100;
+                image.title = file.name;
+                image.src = reader.result;
+                preview.appendChild(image);
+              },
+              false,
+            );
+      
+            reader.readAsDataURL(file);
+          }
+        }
+      
+        if (files) {
+          Array.prototype.forEach.call(files, readAndPreview);
+        }
+      }
+      
+      const picker = document.querySelector("#browse");
+      picker.addEventListener("change", previewFiles);
     
